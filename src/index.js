b617eaccd63a455ece6f6457c4831cd4e481fa74
Ernane Sabino <esabino@stefanini.com>
2021-07-10 14:19:36 -0300

    cadastro de conta e validaçao de cpf existente

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+app.post("/account", (request, response) => {
+
+    const { cpf, name } = request.body;
+
+    // Percorre o array para verificar se o cpf já exite
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if(customerAlreadyExists) {
+        return response.status(400).json({error: "Customer already exists!"});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+
+});
+
+app.listen(3333);
