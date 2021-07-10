eb32b1c58f67d327d6dc4219c2d20201cec30d1a
Ernane Sabino <esabino@stefanini.com>
2021-07-10 16:22:32 -0300

    criando saque na conta

--- src/index.js
+++ src/index.js
@@ -26,6 +26,23 @@ function verifyIfExistsAccountCPF(request, response, next) {
 
 }
 
+function getBalance(statement) {
+
+    // Reduce tranforma (calcula) valores
+    const balance = statement.reduce((acc, operation) => {
+        
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }        
+
+    }, 0);
+
+    return balance;
+
+}
+
 app.post("/account", (request, response) => {
 
     const { cpf, name } = request.body;
@@ -68,6 +85,30 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
         created_at: new Date(),
         type: "credit"
     }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { amount } = request.body;
+
+    const customer = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return response.status(400).json({error: "Insuficient funds!"});
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }; 
 
     customer.statement.push(statementOperation);
 
